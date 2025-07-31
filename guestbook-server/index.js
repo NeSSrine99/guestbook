67c7e0784b16a350ccb8898963c37826c8ee25fb
nesrin <103462967+Mnasrinesrine@users.noreply.github.com>
2025-07-31 11:14:00 +0100

    edit PORT

--- guestbook-server/index.js
+++ guestbook-server/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const fs = require("fs");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
